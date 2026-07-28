2198bb1d222dbcb32dea93b31476d98362688cf0
Sloan Haywood <haywood.sloan@gmail.com>
2026-07-27 20:55:37 -0400

    add pages deploy

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const isUserSite = repositoryName?.endsWith('.github.io')
+const githubPagesBase =
+  repositoryName && !isUserSite ? `/${repositoryName}/` : '/'
+
+export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? githubPagesBase : '/',
+})
